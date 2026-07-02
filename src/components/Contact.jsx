bf35e87452639cac35915406362aa4083c0d4485
El3troX <111356419+El3troX@users.noreply.github.com>
2026-07-03 01:20:46 +0530

    feat: add responsive Contact component with intersection observer animations and marquee footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -38,17 +38,6 @@ const SOCIALS = [
       </svg>
     ),
   },
-  {
-    name: 'Phone',
-    href: 'tel:+919479865784',
-    modifier: 'phone',
-    label: 'Call (+91) 94798 65784',
-    icon: (
-      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-        <path d="M6.62 10.79a15.053 15.053 0 006.59 6.59l2.2-2.2a1.003 1.003 0 011.01-.24c1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
-      </svg>
-    ),
-  },
 ];
 
 export default function Contact() {
